db1b52cacfc0a9f3479a53831d7c9ae27a442b65
Marcos Persico <willian.marcos7@hotmail.com>
2022-12-01 23:31:39 -0300

    header add

--- assets/javascripts/feedback.js
+++ assets/javascripts/feedback.js
@@ -1,17 +1,1 @@
-document.querySelector('#question1').focus()
-
-var menu = document.querySelector('#header-menu');
-var burguer = document.querySelector('.hamburguer');
-var menuBtn = document.getElementsByClassName('menu-btn');
-
-burguer.addEventListener("click", e=>{ 
-    if(menuBtn[1].classList.contains('hidden')){
-        menu.style.transform = "translateY(100%)";
-        document.querySelector('.range-label').style.display ="none"
-    }else if(menuBtn[0].classList.contains('hidden')){
-        menu.style.transform = "translateY(-100%)";
-        document.querySelector('.range-label').style.display ="block"
-    }
-    menuBtn[0].classList.toggle('hidden');
-    menuBtn[1].classList.toggle('hidden');
-})
+document.querySelector('#question1').focus();
